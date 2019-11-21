0a6ecd7b0d4840c13c10d8ad00abcaaeb3097231
gedongdong <dd.ge@signway.cn>
2019-11-21 21:51:32 +0800

    nio相关配置

--- server/src/main/java/com/jelly/controller/IndexController.java
+++ server/src/main/java/com/jelly/controller/IndexController.java
@@ -1,10 +1,18 @@
 package com.jelly.controller;
 
+import com.jelly.constant.Constants;
+import com.jelly.enums.StatusEnum;
+import com.jelly.res.BaseResponse;
 import com.jelly.server.EasyIMServer;
+import com.jelly.vo.req.SendMsgReqVO;
+import com.jelly.vo.res.SendMsgResVO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
@@ -15,5 +23,21 @@ public class IndexController {
 
     @Autowired
     private CounterService counterServicel;
+
+    @ApiOperation("服务端发送消息")
+    @RequestMapping(value = "sendMsg", method = RequestMethod.POST)
+    @RequestBody
+    public BaseResponse<SendMsgResVO> sendMsg(@RequestBody SendMsgReqVO sendMsgReqVO) {
+        BaseResponse<SendMsgResVO> res = new BaseResponse<>();
+        easyIMServer.sendMsg(sendMsgReqVO);
+        counterServicel.increment(Constants.COUNTER_SERVER_PUSH_COUNT);
+
+        SendMsgResVO sendMsgResVO = new SendMsgResVO();
+        sendMsgReqVO.setMsg("OK");
+        res.setCode(StatusEnum.SUCCESS.getCode());
+        res.setMessage(StatusEnum.SUCCESS.getMessage());
+        res.setDataBody(sendMsgResVO);
+        return res;
+    }
 
 }
--- server/src/main/java/com/jelly/server/EasyIMServer.java
+++ server/src/main/java/com/jelly/server/EasyIMServer.java
@@ -1,7 +1,19 @@
 package com.jelly.server;
 
+import com.jelly.vo.req.SendMsgReqVO;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EasyIMServer {
+    private final static Logger LOGGER = LoggerFactory.getLogger(EasyIMServer.class);
+
+    private EventLoopGroup boss = new NioEventLoopGroup();
+    private EventLoopGroup work = new NioEventLoopGroup();
+
+    public void sendMsg(SendMsgReqVO sendMsgReqVO) {
+    }
 }
